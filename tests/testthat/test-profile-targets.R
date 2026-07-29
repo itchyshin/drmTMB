dc3651d4873db6ae2325e58e2538afe7592278aa
Shinichi Nakagawa <itchyshin@gmail.com>
2026-07-29 10:02:55 -0600

    feat: add zero-one-beta phylo q1 point-fit route

--- tests/testthat/test-profile-targets.R
+++ tests/testthat/test-profile-targets.R
@@ -461,6 +461,7 @@ expect_profile_target_contract <- function(targets) {
         "missing_tmb_parameter",
         "point_fit_only_count_q2",
         "point_fit_only_count_sigma_interaction",
+        "point_fit_only_zero_one_beta_phylo_q1",
         "derived_target",
         "derived_unstructured_correlation"
       )
